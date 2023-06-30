a26c6d55c4ca49b06ef8297d738e8815b5895bf4
Dzmitry <55855155+dzmitryZhuk@users.noreply.github.com>
2023-06-30 11:54:29 +0300

    remove useless functions

--- client_functions.h
+++ client_functions.h
@@ -7,7 +7,5 @@
 
 filesize_t getFileSize(const std::string& path); 
 std::string readText(const std::string& path);  // reads text from file
-// std::string addCommand(std::string& text, const Command& command);  // adds command in begin of text
-// std::string commandToString(Command command);   // create string from command (needs for function addCommand)
 
 #endif
